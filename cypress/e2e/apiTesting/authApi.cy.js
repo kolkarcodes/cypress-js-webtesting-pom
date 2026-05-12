91e4e5ae2e15b10889fbbc08b69317e2e4757023
kolkarcodes <kolkar.codes@gmail.com>
2026-05-12 12:02:31 +0100

    fixed the authApi test issue

--- cypress/e2e/apiTesting/authApi.cy.js
+++ cypress/e2e/apiTesting/authApi.cy.js
@@ -3,10 +3,10 @@ describe('Auth API Test',()=>{
     it('should login successfully with valid credentials', () => {
         cy.request({
             method: 'POST',
-            url: 'https://api.practicesoftwaretesting.com/auth/login',
+            url: 'https://api.practicesoftwaretesting.com/users/login',
             body: {
-                email: 'customer@practicesoftwaretesting.com',
-                password: 'welcome01'
+                "email":"customer@practicesoftwaretesting.com",
+                "password":"welcome01"
             }
         }).then((response) => {
             expect(response.status).to.eq(200);
